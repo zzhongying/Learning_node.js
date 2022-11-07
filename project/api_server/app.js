a0eceec150a20936d5844f05d0686abf4709171e
zzhongying <1417104233@qq.com>
2022-11-07 15:46:32 +0800

    modified:   project/api_server/app.js 	modified:   project/api_server/package-lock.json 	modified:   project/api_server/package.json 	new file:   project/api_server/schema/user.js

--- project/api_server/app.js
+++ project/api_server/app.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const joi = require('joi')
 const userRouter = require('./router/user')
 const cors = require('cors')
 
@@ -8,9 +9,31 @@ const app  = express()
 //配置解析表单数据的中间件,只能解析application/x-www-form-urlencoded格式的表单数据,必须放在userRouter前面
 app.use(express.urlencoded({extended:false}))
 
+//一定要在路由之前封装错误处理函数
+app.use((req,res,next)=>{
+  //status=1表示默认值为1，失败 
+  res.cc = function (err,status=1) {     //全局中间件，为res挂载错误处理函数cc
+    res.send({
+      status,
+      message: err instanceof Error ? err.message :err
+    })
+  }
+
+  next()
+})
+
+
+
 //配置跨域中间件
 app.use(cors())
 app.use('/api',userRouter)
+
+//定义全局错误级别的中间件，当当个路由下的错误没有被捕获时，由全局捕获错误
+app.use((err,req,res,next)=>{
+  if(err instanceof joi.ValidationError) return res.cc(err)
+  //未知的错误
+  res.cc(err)
+})
 
 
 app.listen(3007,function () {
---
+++ project/api_server/schema/user.js
@@ -0,0 +1,24 @@
+//导入定义验证规则的包
+const joi = require('joi')
+
+/**
+ * string() 值必须是字符串
+ * alphanum() 值只能是包含 a-zA-Z0-9 的字符串
+ * min(length) 最小长度
+ * max(length) 最大长度
+ * required() 值是必填项，不能为 undefined
+ * pattern(正则表达式) 值必须符合正则表达式的规则
+ */
+
+//定义用户验证规则
+const usernamr = joi.string().alphanum().min(1).max(10).required()
+const password = joi.string().pattern(/^[\S]{6,12}$/).required()
+
+//定义验证登录注册表单数据的规则
+exports.reg_login_schema = {
+    body:{
+       usernamr,
+       password, 
+    },
+
+}
