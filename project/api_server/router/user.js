03a946ee1fa884264d3c5977057b1d5bfe5b880c
zzhongying <1417104233@qq.com>
2022-11-07 15:45:48 +0800

    modified:   project/api_server/router/user.js 	modified:   project/api_server/router_handler/user.js 	modified:   project/api_server/app.js 	modified:   project/api_server/package-lock.json 	modified:   project/api_server/package.json 	project/api_server/schema/

--- project/api_server/router/user.js
+++ project/api_server/router/user.js
@@ -3,12 +3,18 @@ const express = require('express')
 //导入用户路由处理函数
 const user_Handler = require('../router_handler/user')
 
+//导入验证数据的中间件
+const expressJoi = require('@escook/express-joi')
+//导入需要验证规则对象
+const { reg_login_schema} = require('../schema/user')
+
+
 const router = express.Router()
 
-//注册新用户
-router.post('/regust', user_Handler.regUser)
+//注册新用户,并调用规则检验中间件进行合法新验证
+router.post('/regust',expressJoi(reg_login_schema) ,user_Handler.regUser)
 
 //登录
-router.post('/login', user_Handler.logUser)
+router.post('/login', expressJoi(reg_login_schema),user_Handler.logUser)
 
 module.exports = router
--- project/api_server/router_handler/user.js
+++ project/api_server/router_handler/user.js
@@ -10,22 +10,24 @@ exports.regUser = (req,res)=>{
     const userinfo = req.body
    
     //对表单数据进行合法性判断
-    if(!userinfo.username || !userinfo.password){
-        console
-        return res.send({status:1,message:'用户名或密码不合法'})
-    }
+    // if(!userinfo.username || !userinfo.password){
+    //     console
+    //     return res.send({status:1,message:'用户名或密码不合法'})
+    // }
 
     //定义SQL语句，查询用户名是否被占用
     const sqlStr = 'select * from ev_users where username=?'
     db.query(sqlStr,userinfo.username,(err,results)=>{
         //执行SQL语句失败
         if(err){
-            return res.send({status:1,message:err.message})
+            // return res.send({status:1,message:err.message})
+            return res.cc(err)
         }
 
         //判断用户名是否被占用，SQL是查询语句，results必然是一个数组
         if(results.length>0){
-            return res.send({status:1,message: '用户名已经存在！'})
+            // return res.send({status:1,message: '用户名已经存在！'})
+            return res.cc('用户名已经存在！')
         }
 
         //TODO：用户名可用，对输入的密码进行加密
@@ -37,22 +39,61 @@ exports.regUser = (req,res)=>{
         const sql = 'insert into ev_users set ?'
         db.query(sql,{username:userinfo.username,password:userinfo.password},(err,results)=>{
             if(err){
-                return res.send({status:1,message:err.message})
+                // return res.send({status:1,message:err.message})
+                return res.cc(err)
             }
             
             //判断影响行数是否为1
-            if(results.affectedRows !== 1) return res.send({status:1,message:'注册用户失败，请稍后再试！'})
+            if(results.affectedRows !== 1) {
+                //return res.send({status:1,message:'注册用户失败，请稍后再试！'})
+                return res.cc('注册用户失败，请稍后再试！')
+            }
 
             //注册用户成功
-            res.send({status:0,message:'注册用户成功！'})
+            // res.send({status:0,message:'注册用户成功！'})
+            return res.cc('注册用户成功！',0)  //注册成功后，将状态变为0
         })
     })
 
   
 }
 
+
 //用户登录
 exports.logUser = (req,res)=>{
-    res.send('login OK')
+
+    //接收表单数据
+
+    const userinfo = req.body
+    //定义SQL语句
+    const sql = 'select * from ev_users where usernmae=?'
+
+    //执行SQL语句
+    db.query(sql,userinfo.username,(err,results)=>{
+        if(err) return res.cc  //执行sql语句失败
+
+        //执行sql成功，但是没有获取到数据
+        if(results.length !=1){
+            return res.cc('登录失败')
+        }
+
+        //判断登录密码，使用`bcrypt.compareSync`（用户提交的密码，数据库中的密码）方法比较密码是否一致
+        const compareSync = bcrypt.compareSync(userinfo.password,results[0].password)
+
+        //如果对比结果为fals，则输入密码错误
+        if(compareSync === false){
+            return res.send('输入密码错误，登录失败')
+        }
+        //TODO：生成token，核心注意点：在生成token字符串时，一定要剔除密码和头像的值
+        
+
+        
+        
+        res.send('login OK')
+    
+    
+    })
+
+ 
 }
 
